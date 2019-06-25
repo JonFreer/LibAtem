b18ef67e2bc0cec8c520e99338fffde551aee8b7
Julian Waller <git@julusian.co.uk>
2019-06-25 19:42:16 +0100

    feat: multiview updates

--- LibAtem/Commands/Settings/Multiview/MultiviewPropertiesSetCommand.cs
+++ LibAtem/Commands/Settings/Multiview/MultiviewPropertiesSetCommand.cs
@@ -7,15 +7,13 @@ using LibAtem.Serialization;
 
 namespace LibAtem.Commands.Settings.Multiview
 {
-    [CommandName("CMvP", 8)]
+    [CommandName("CMvP", 4)]
     public class MultiviewPropertiesSetCommand : SerializableCommandBase
     {
         [Flags]
         public enum MaskFlags
         {
             Layout = 1 << 0,
-            SafeAreaEnabled = 1 << 1,
-            ProgramPreviewSwapped = 1 << 2,
         }
 
         [Serialize(0), Enum8]
@@ -25,10 +23,6 @@ namespace LibAtem.Commands.Settings.Multiview
         public uint MultiviewIndex { get; set; }
         [Serialize(2), Enum8]
         public MultiViewLayout Layout { get; set; }
-        [Serialize(3), Bool]
-        public bool SafeAreaEnabled { get; set; }
-        [Serialize(4), Bool]
-        public bool ProgramPreviewSwapped { get; set; }
 
         public override IEnumerable<MacroOpBase> ToMacroOps()
         {
@@ -39,10 +33,6 @@ namespace LibAtem.Commands.Settings.Multiview
 
                 yield return new MultiViewLayoutMacroOp { MultiViewIndex = MultiviewIndex, Layout = layout };
             }
-            if (Mask.HasFlag(MaskFlags.SafeAreaEnabled))
-                yield return null;
-            if (Mask.HasFlag(MaskFlags.ProgramPreviewSwapped))
-                yield return null;
         }
     }
 }
---
+++ LibAtem/Commands/Settings/Multiview/MultiviewWindowSafeAreaCommand.cs
@@ -0,0 +1,17 @@
+﻿using LibAtem.Serialization;
+
+namespace LibAtem.Commands.Settings.Multiview
+{
+    [CommandName("SaMw", 4)]
+    public class MultiviewWindowSafeAreaCommand : SerializableCommandBase
+    {
+        [CommandId]
+        [Serialize(0), UInt8]
+        public uint MultiviewIndex { get; set; }
+        [CommandId]
+        [Serialize(1), UInt8Range(0, 15)] // TODO - more specific than that?
+        public uint WindowIndex { get; set; }
+        [Serialize(2), Bool]
+        public bool SafeAreaEnabled { get; set; }
+    }
+}
