be95c57ab272d3a1b7a8d2115b615e1c29963e04
JonFreer <j.freer@hotmail.co.uk>
2020-04-07 16:38:56 +0100

    Updated MediaPoolClipLength

--- LibAtem/Commands/Media/MediaPoolSettingsSetCommand.cs
+++ LibAtem/Commands/Media/MediaPoolSettingsSetCommand.cs
@@ -1,28 +1,32 @@
+using LibAtem.Serialization;
 using System.Collections.Generic;
 
 namespace LibAtem.Commands.Media
 {
-    [CommandName("CMPS", CommandDirection.ToServer), NoCommandId]
-    public class MediaPoolSettingsSetCommand : ICommand
+    [CommandName("CMPS", CommandDirection.ToServer, 8), NoCommandId]
+    public class MediaPoolSettingsSetCommand : SerializableCommandBase
     {
-        public List<uint> MaxFrames { get; set; }
+        [Serialize(0), UInt16]
 
-        public void Serialize(ByteArrayBuilder cmd)
-        {
-            foreach (uint fr in MaxFrames)
-                cmd.AddUInt16(fr);
+        public uint Clip1MaxLength { get; set; }
+        //  public List<uint> MaxFrames { get; set; }
 
-            cmd.PadToNearestMultipleOf4();
-        }
+        // public void Serialize(ByteArrayBuilder cmd)
+        // {
+        //      foreach (uint fr in MaxFrames)
+        //          cmd.AddUInt16(fr);
+        //
+        //      cmd.PadToNearestMultipleOf4();
+        //  }
 
-        public void Deserialize(ParsedByteArray cmd)
-        {
-            MaxFrames = new List<uint>();
+        //  public void Deserialize(ParsedByteArray cmd)
+        //   {
+        //      MaxFrames = new List<uint>();
+        //
+        //       for (int i = 0; i < cmd.BodyLength; i += 2)
+        //           MaxFrames.Add(cmd.GetUInt16());
 
-            for (int i = 0; i < cmd.BodyLength; i += 2)
-                MaxFrames.Add(cmd.GetUInt16());
-
-            cmd.SkipToNearestMultipleOf4();
-        }
+        //      cmd.SkipToNearestMultipleOf4();
+        //  }
     }
 }
